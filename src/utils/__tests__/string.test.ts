888bab2cf2ab345549714749a816883a2f01221e
Carlos <casantosmu@gmail.com>
2022-11-14 21:03:05 +0100

    Added getHostname to string on /utils

--- src/utils/__tests__/string.test.ts
+++ src/utils/__tests__/string.test.ts
@@ -1,12 +1,47 @@
-import { capitalizeFirstWord } from "../string";
+import string from "../string";
 
-describe("Given a capitalizeFirstWord", () => {
+describe("Given a capitalizeFirstWord function", () => {
   describe("When it receives 'testing word'", () => {
     test("Then it should return 'Testing word'", () => {
       const lowercaseString = "testing word";
       const expectedResult = "Testing word";
 
-      const result = capitalizeFirstWord(lowercaseString);
+      const result = string.capitalizeFirstWord(lowercaseString);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+});
+
+describe("Given a getHostname function", () => {
+  describe("When it receives 'http://www.host.name/some/paths?query=true&more=true'", () => {
+    test("Then it should return 'www.host.name'", () => {
+      const fullUrl = "http://www.host.name/some/paths?query=true&more=true";
+      const expectedHostname = "www.host.name";
+
+      const result = string.getHostname(fullUrl);
+
+      expect(result).toBe(expectedHostname);
+    });
+  });
+
+  describe("When it receives 'https://host.name/'", () => {
+    test("Then it should return 'www.host.name'", () => {
+      const fullUrl = "https://host.name/";
+      const expectedHostname = "host.name";
+
+      const result = string.getHostname(fullUrl);
+
+      expect(result).toBe(expectedHostname);
+    });
+  });
+
+  describe("When it receives an empty string: ''", () => {
+    test("Then it should return an empty string: ''", () => {
+      const recivedString = "";
+      const expectedResult = "";
+
+      const result = string.getHostname(recivedString);
 
       expect(result).toBe(expectedResult);
     });
--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,2 +1,12 @@
-export const capitalizeFirstWord = (string: string): string =>
+const capitalizeFirstWord = (string: string): string =>
   string.charAt(0).toUpperCase() + string.slice(1);
+
+const getHostname = (string: string): string =>
+  string.trim() === "" ? "" : new URL(string).hostname;
+
+const string = {
+  capitalizeFirstWord,
+  getHostname,
+};
+
+export default string;
